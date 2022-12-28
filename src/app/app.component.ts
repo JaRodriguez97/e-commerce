e0d44e0db2cfe12142ddaf01510ee719dddc93a6
JaRodriguez97 <cristi542012@gmail.com>
2022-12-28 12:02:07 -0500

    creatulogotipo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
-
+import { DOCUMENT } from '@angular/common';
+import {
+  Component,
+  HostListener,
+  Inject,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import {
+  faUser,
+  faEye,
+  faHeart,
+  faShoppingCart,
+  faStar,
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'eCommerce';
+  navbar!: Element;
+  faHeart = faHeart;
+  faShoppingCart = faShoppingCart;
+  faUser = faUser;
+
+  constructor(
+    private renderer: Renderer2,
+    private activatedRoute: ActivatedRoute,
+    public router: Router,
+    // public localStorageService: LocalStorageService,
+    // private spinner: NgxSpinnerService,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+
+  @HostListener('window:scroll')
+  scrolling(): void {
+    this.navbar.classList.remove('active');
+
+    if (window.scrollY > 500)
+      this.document.querySelector('header')!.classList.add('active');
+    else this.document.querySelector('header')!.classList.remove('active');
+  }
+
+  ngOnInit() {
+    this.navbar = this.document.querySelector('.navbar')!;
+    console.log(
+      '🚀 ~ file: app.component.ts:29 ~ AppComponent ~ this.navbar',
+      this.navbar
+    );
+
+    // this.renderer.listen()
+
+    // this.document.querySelector('#menu-bar')!.click = () => {
+    //   this.navbar.classList.toggle('active');
+    // };
+    // this.document.querySelector('#close')!.onclick = () => {
+    //   this.navbar.classList.remove('active');
+    // };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AboutComponent } from '@components/about/about.component';
 import { ContactComponent } from '@components/contact/contact.component';
@@ -6,6 +6,7 @@ import { IconsContainerComponent } from '@components/icons-container/icons-conta
 import { InicioComponent } from '@components/Inicio/inicio/inicio.component';
 import { ProductsComponent } from '@components/products/products.component';
 import { ReviewComponent } from '@components/review/review.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,8 +24,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
+    FontAwesomeModule,
   ],
-  // schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent],
 })
